3eae7365db9ffd582c3fa05e9c5e450da210ff97
Salman-sajib <sfsajib@gmail.com>
2024-08-22 21:39:16 +0600

    counter app, initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Counter from "./components/Counter.vue";
+</script>
+
+<template>
+  <div class="app bg-slate-50">
+    <header class="justify-start">
+      <h1 class="text-4xl font-semibold text-center pt-5">Vue Playground</h1>
+    </header>
+    <div class="min-h-screen flex flex-col items-center justify-center">
+      <Counter />
+    </div>
+  </div>
+</template>
---
+++ src/components/Counter.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+const decrement = () => {
+  count.value--;
+};
+const reset = () => {
+  count.value = 0;
+};
+</script>
+
+<template>
+  <div class="container mx-auto flex flex-col items-center justify-center pt-3">
+    <h1 class="text-5xl font-medium text-center mt-5">Counter App</h1>
+    <p class="text-3xl py-5">
+      Count is :
+      <span
+        :class="{ 'text-blue-500': count > 0, 'text-red-500': count < 0 }"
+        >{{ count }}</span
+      >
+    </p>
+    <div class="flex items-center gap-2 pt-3">
+      <button
+        @click="increment"
+        class="bg-green-700 text-slate-50 px-4 py-2 rounded-md"
+      >
+        Increment
+      </button>
+      <button
+        @click="reset"
+        class="bg-red-700 text-slate-50 px-4 py-2 rounded-md"
+      >
+        Reset
+      </button>
+      <button
+        @click="decrement"
+        class="bg-blue-700 text-slate-50 px-4 py-2 rounded-md"
+      >
+        Decrement
+      </button>
+    </div>
+  </div>
+</template>
