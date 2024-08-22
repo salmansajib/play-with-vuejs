4b4d9d09fdf6bb922c6ea95d6d6f4b9a546c5b48
Salman-sajib <sfsajib@gmail.com>
2024-08-22 23:48:20 +0600

    modified the counter component

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@ import Counter from "./components/Counter.vue";
 
 <template>
   <div class="app bg-slate-50">
-    <header class="justify-start">
-      <h1 class="text-4xl font-semibold text-center pt-5">Vue Playground</h1>
+    <header class="bg-slate-900">
+      <h1 class="text-4xl font-semibold text-center text-slate-50 py-5">
+        Vue Playground
+      </h1>
     </header>
     <div class="min-h-screen flex flex-col items-center justify-center">
       <Counter />
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const count = ref(0);
 
@@ -12,19 +12,26 @@ const decrement = () => {
 const reset = () => {
   count.value = 0;
 };
+const doubleCount = computed(() => {
+  return count.value * 2;
+});
+const tripleCount = computed(() => {
+  return count.value * 3;
+});
 </script>
 
 <template>
   <div class="container mx-auto flex flex-col items-center justify-center pt-3">
     <h1 class="text-5xl font-medium text-center mt-5">Counter App</h1>
-    <p class="text-3xl py-5">
+    <p class="text-3xl pt-10 font-semibold">
       Count is :
-      <span
-        :class="{ 'text-blue-500': count > 0, 'text-red-500': count < 0 }"
-        >{{ count }}</span
-      >
+      <span :class="count >= 0 ? 'text-blue-500' : 'text-red-500'">{{
+        count
+      }}</span>
     </p>
-    <div class="flex items-center gap-2 pt-3">
+    <p class="text-3xl pt-5 font-semibold">Double Count : {{ doubleCount }}</p>
+    <p class="text-3xl pt-5 font-semibold">Triple Count : {{ tripleCount }}</p>
+    <div class="flex items-center gap-2 pt-5">
       <button
         @click="increment"
         class="bg-green-700 text-slate-50 px-4 py-2 rounded-md"
